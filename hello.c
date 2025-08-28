43aa6d51cab4356a30271c86c416202cafefe1ed
Kai White <kwhite42@huskers.unl.edu>
2025-08-28 12:59:04 -0500

    initial comit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: NAME
+ *         EMAIL
+ * Date: 20xx/xx/xx
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Kai White\n");
+
+  return 0;
+}
